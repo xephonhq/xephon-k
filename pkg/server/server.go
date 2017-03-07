2eea8a7234796d3402c80d75da86b728144221ce
at15 <at15@outlook.com>
2017-03-07 00:22:54 -0800

    [server] Add ServiceFactory interface #6
    - use service factory to create endpoint, encode function, decode
    function, was planning to put those in service interface, then it turns
    out every middleware have to implement these three functions
    - service interface is created for letting makeEndpoint take all types
    of service, and the `ServiceName` method is used to avoid passing non
    service objects into `makeEndpoint`
    - NOTE: it seems even my own type `decodeHTTPRequest` is same as
    go-kit's `DecodeRequestFunc`, they are not switchable

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -16,21 +16,23 @@ func (Server) Start() {
 	// FIXME: the context part in html is also outdated
 	// ctx := context.Background()
 	infoSvc := infoService{}
+	infoSvcHTTPFactory := infoServiceHTTPFactory{}
 
+	// infoHandler := httptransport.NewServer(
+	// 	// ctx,
+	// 	makeInfoEndpoint(infoSvc),
+	// 	decodeInfoRequest,
+	// 	encodeResponse,
+	// )
 	infoHandler := httptransport.NewServer(
-		// ctx,
-		makeInfoEndpoint(infoSvc),
-		decodeInfoRequest,
-		encodeResponse,
+		infoSvcHTTPFactory.makeEndpoint(infoSvc),
+		infoSvcHTTPFactory.makeDecode(),
+		infoSvcHTTPFactory.makeEncode(),
 	)
 
 	http.Handle("/info", infoHandler)
 	log.Infof("start serving on 0.0.0.0:%d", 8080)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func decodeInfoRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	return infoRequest{}, nil
 }
 
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
