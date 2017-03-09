fbeca898c0e683fe96121b7a46d5468ae786b8f3
at15 <at15@outlook.com>
2017-03-08 23:54:04 -0800

    [store][mem] Init WriteSeries (but failed) #3
    - use merge sort to merge new points with old points (let's just assume
    old data is flushed to cassandra so the memory can always fit)
    - [ ] FIXME: the length is not updated correctly, or maybe the merge
    itself is wrong

--- pkg/storage/memory/data.go
+++ pkg/storage/memory/data.go
@@ -1,9 +1,13 @@
 package memory
 
 import (
-	"github.com/xephonhq/xephon-k/pkg/common"
+	"sort"
 	"sync"
+
+	"github.com/xephonhq/xephon-k/pkg/common"
 )
+
+var initPointsLength = 100
 
 type SeriesID string
 
@@ -13,13 +17,61 @@ type Data map[SeriesID]IntSeriesStore
 type IntSeriesStore struct {
 	mu     sync.RWMutex
 	series common.IntSeries
+	length int
 }
 
-func (store IntSeriesStore) WriteSeries(newSeries common.IntSeries) {
+func NewIntSeriesStore() *IntSeriesStore {
+	series := common.IntSeries{}
+	series.Points = make([]common.IntPoint, initPointsLength)
+	return &IntSeriesStore{series: series, length: 0}
+}
+
+func (store *IntSeriesStore) WriteSeries(newSeries common.IntSeries) {
 	store.mu.Lock()
 	defer store.mu.Unlock()
+
 	// merge the old series with new series
 	// TODO: efficient merge sort
-	// store.series should already be sorted
+	// TODO: actually we can remove duplicate when merge by comparing with previous point
 
+	// store.series should already be sorted, so we only sort the newSeries
+	sort.Sort(common.ByTime(newSeries.Points))
+	i := 0
+	j := 0
+	k := 0
+	// NOTE: we can't use len(store.series.Points) because there might be duplicate
+	oldLength := store.length
+	newLength := len(newSeries.Points)
+	points := make([]common.IntPoint, oldLength+newLength)
+	for i < oldLength && j < newLength {
+		if store.series.Points[i].TimeNano < newSeries.Points[j].TimeNano {
+			points[k] = store.series.Points[i]
+			i++
+		} else if store.series.Points[i].TimeNano == newSeries.Points[j].TimeNano {
+			// if there is duplicate, overwrite the old point with new point
+			points[k] = store.series.Points[j]
+			i++
+			j++
+		} else {
+			points[k] = store.series.Points[j]
+			j++
+		}
+		k++
+	}
+	store.length = k
+
+	// copy what is left, should only have one array left
+	// https://github.com/golang/go/wiki/SliceTricks
+	if i < oldLength {
+		points = append(points, store.series.Points[i:]...)
+		// TODO: will there be +1 problem?
+		store.length = k + oldLength - i
+	}
+	if j < newLength {
+		points = append(points, newSeries.Points[j:]...)
+		store.length = k + newLength - i
+	}
+
+	// TODO: maybe using pool is a good idea since there are a lot of merge when inserting series
+	store.series.Points = points
 }
--- pkg/storage/memory/data_test.go
+++ pkg/storage/memory/data_test.go
@@ -1,7 +1,29 @@
 package memory
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/xephonhq/xephon-k/pkg/common"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestIntSeriesStore_WriteSeries(t *testing.T) {
-
+	asst := assert.New(t)
+	store := NewIntSeriesStore()
+	asst.Equal(0, store.length)
+	tags := make(map[string]string)
+	tags["os"] = "ubuntu"
+	p1 := common.IntPoint{TimeNano: 1359788400002, V: 1}
+	p2 := common.IntPoint{TimeNano: 1359788400003, V: 2}
+	ps1 := []common.IntPoint{p1, p2}
+	s1 := common.IntSeries{Name: "cpi", Tags: tags, Points: ps1}
+	store.WriteSeries(s1)
+	asst.Equal(2, store.length)
+	p3 := common.IntPoint{TimeNano: 1359788400001, V: 1}
+	p4 := common.IntPoint{TimeNano: 1359788400004, V: 2}
+	ps2 := []common.IntPoint{p3, p4}
+	s2 := common.IntSeries{Name: "cpi", Tags: tags, Points: ps2}
+	store.WriteSeries(s2)
+	// FIXME: this test broke
+	asst.Equal(4, store.length)
 }
