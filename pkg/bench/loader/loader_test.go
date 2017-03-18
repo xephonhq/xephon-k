98170a0d1417bc28020379e0b81ab4ae7c726def
at15 <at15@outlook.com>
2017-03-18 16:08:50 -0700

    [bench] InfluxDB still poor when run locally #15
    - download the latest version and run it locally, the performance is
      still very poor, 1/100 of XephonK, and when all the request is 404
    (don't create the database) the speed is faster than XephonK
    - [ ] TODO: do I need to create schema for table? or I am doing sth
      wrong

--- pkg/bench/loader/loader_test.go
+++ pkg/bench/loader/loader_test.go
@@ -18,10 +18,22 @@ func TestHTTPLoader_Run(t *testing.T) {
 	//INFO[0010] total request 119 pkg=k.b.reporter
 	//INFO[0010] fastest 231350986 pkg=k.b.reporter
 	//INFO[0010] slowest 714779592 pkg=k.b.reporter
-	ld := NewHTTPLoader(NewConfig(bench.DBXephonK), &reporter.BasicReporter{}) //
+	//ld := NewHTTPLoader(NewConfig(bench.DBXephonK), &reporter.BasicReporter{}) //
 	//INFO[0010] total request 11742 pkg=k.b.reporter
 	//INFO[0010] fastest 1074761 pkg=k.b.reporter
 	//INFO[0010] slowest 36497286 pkg=k.b.reporter
+
+	// try with local influxdb
+	ld := NewHTTPLoader(NewConfig(bench.DBInfluxDB), &reporter.BasicReporter{})
+	// when db is not created, it seems pretty good
+	//INFO[0010] total request 14446 pkg=k.b.reporter
+	//INFO[0010] fastest 231121 pkg=k.b.reporter
+	//INFO[0010] slowest 48644323 pkg=k.b.reporter
+	// well for local
+	//INFO[0010] total request 125 pkg=k.b.reporter
+	//INFO[0010] fastest 192001022 pkg=k.b.reporter
+	//INFO[0010] slowest 549661074 pkg=k.b.reporter
+	//INFO[0010] loader finished pkg=k.b.loader
 	ld.Run()
 }
 
