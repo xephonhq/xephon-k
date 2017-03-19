f382862cbface7ef4a7bb33d38e4ea3bd883bcfe
at15 <at15@outlook.com>
2017-03-18 17:53:45 -0700

    [bench] Can't push to Prometheus #17
    - it is a pull style system and have to use the push gateway

---
+++ script/prometheus/README.md
@@ -0,0 +1,5 @@
+# Prometheus
+
+## Ref 
+
+https://github.com/at15/hadoop-spark-perf/tree/master/provision/monitor
---
+++ script/prometheus/up.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# get the script path http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+ORIGINAL_WD=${PWD}
+cd ${SCRIPTPATH}
+
+cd prometheus-1.5.2.linux-amd64
+./prometheus -config.file=prometheus.yml
