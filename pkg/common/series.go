a87a79ea0d1643e56a5afffcb685335330b58997
at15 <at15@outlook.com>
2017-05-24 17:51:26 -0700

    [common][series] Use SeriesMeta as anonymous field
    - [ ] TODO: this breaks most existing tests
    - [ ] TODO: it has problem for JSON marshal and unmarshal because it
    will not put the fields of the anonymous struct in top level.
    ````
    [
      {
        "SeriesMeta": {
             "name": "cpu.idle"
             ....
         },
        "points": [[1123123, 1], [12312, 2]]
      }
    ]
    ````

--- pkg/common/series.go
+++ pkg/common/series.go
@@ -36,49 +36,50 @@ type SeriesMeta struct {
 }
 
 type RawSeries struct {
-	id         SeriesID
-	Name       string            `json:"name"`
-	Tags       map[string]string `json:"tags"`
-	SeriesType int               `json:"type"`
-	Precision  time.Duration     `json:"precision"`
-	Points     json.RawMessage   `json:"points"`
+	SeriesMeta
+	Points json.RawMessage `json:"points"`
 }
 
 type IntSeries struct {
-	id         SeriesID
-	Name       string            `json:"name"`
-	Tags       map[string]string `json:"tags"`
-	SeriesType int               `json:"type,omitempty"`
-	Precision  time.Duration     `json:"precision,omitempty"`
-	Points     []IntPoint        `json:"points"`
+	SeriesMeta
+	Points []IntPoint `json:"points"`
 }
 
 type DoubleSeries struct {
-	id         SeriesID
-	Name       string            `json:"name"`
-	Tags       map[string]string `json:"tags"`
-	SeriesType int               `json:"type,omitempty"`
-	Precision  time.Duration     `json:"precision,omitempty"`
-	Points     []DoublePoint     `json:"points"`
+	SeriesMeta
+	Points []DoublePoint `json:"points"`
 }
 
 // TODO: int series of other precision, maybe we should add millisecond to the default function as well
 func NewIntSeries(name string) *IntSeries {
-	return &IntSeries{
-		Name:       name,
-		Tags:       make(map[string]string, 1),
-		SeriesType: TypeIntSeries,
-		Precision:  time.Millisecond,
-	}
+	// return &IntSeries{
+	// 	Name:       name,
+	// 	Tags:       make(map[string]string, 1),
+	// 	SeriesType: TypeIntSeries,
+	// 	Precision:  time.Millisecond,
+	// }
+
+	s := IntSeries{}
+	s.Name = name
+	s.Tags = map[string]string{}
+	s.SeriesType = TypeIntSeries
+	s.Precision = time.Millisecond
+	return &s
 }
 
 func NewDoubleSeries(name string) *DoubleSeries {
-	return &DoubleSeries{
-		Name:       name,
-		Tags:       make(map[string]string, 1),
-		SeriesType: TypeDoubleSeries,
-		Precision:  time.Millisecond,
-	}
+	// return &DoubleSeries{
+	// 	Name:       name,
+	// 	Tags:       make(map[string]string, 1),
+	// 	SeriesType: TypeDoubleSeries,
+	// 	Precision:  time.Millisecond,
+	// }
+	s := DoubleSeries{}
+	s.Name = name
+	s.Tags = map[string]string{}
+	s.SeriesType = TypeDoubleSeries
+	s.Precision = time.Millisecond
+	return &s
 }
 
 func (series *SeriesMeta) GetName() string {
@@ -94,64 +95,6 @@ func (series *SeriesMeta) GetSeriesType() int {
 }
 
 func (series *SeriesMeta) GetSeriesID() SeriesID {
-	if series.id == 0 {
-		series.id = Hash(series)
-	}
-	return series.id
-}
-
-func (series *RawSeries) GetName() string {
-	return series.Name
-}
-
-func (series *RawSeries) GetTags() map[string]string {
-	return series.Tags
-}
-
-func (series *RawSeries) GetSeriesType() int {
-	return series.SeriesType
-}
-
-func (series *RawSeries) GetSeriesID() SeriesID {
-	if series.id == 0 {
-		series.id = Hash(series)
-	}
-	return series.id
-}
-
-func (series *IntSeries) GetName() string {
-	return series.Name
-}
-
-func (series *IntSeries) GetTags() map[string]string {
-	return series.Tags
-}
-
-func (series *IntSeries) GetSeriesType() int {
-	// TODO: do we still need the variable if we return constant, and should we check consistency for that
-	return TypeIntSeries
-}
-
-func (series *IntSeries) GetSeriesID() SeriesID {
-	if series.id == 0 {
-		series.id = Hash(series)
-	}
-	return series.id
-}
-
-func (series *DoubleSeries) GetName() string {
-	return series.Name
-}
-
-func (series *DoubleSeries) GetTags() map[string]string {
-	return series.Tags
-}
-
-func (series *DoubleSeries) GetSeriesType() int {
-	return TypeDoubleSeries
-}
-
-func (series *DoubleSeries) GetSeriesID() SeriesID {
 	if series.id == 0 {
 		series.id = Hash(series)
 	}
