64d9b4d20226e0e226f652c2fa8835d83dc4e47e
at15 <at15@outlook.com>
2017-05-25 11:01:18 -0700

    [play][protobuf] Add nullable to remove ptr #42
    - by default, the generated struct use pointer for non primitive field,
    i.e. the `Points` in `IntSeries` is `[]*IntPoint`, which cause more
    problem for GC I believe
    - change the field name in .proto to lower case, since gogo with
    Ucfirst them, and use the original (lower case) field name for JSON
    as `json:"lower,omitempty"`

--- playground/common/common_test.go
+++ playground/common/common_test.go
@@ -20,3 +20,48 @@ func TestIntPoint_Marshal(t *testing.T) {
 	bj, err := json.Marshal(p)
 	t.Log(string(bj))
 }
+
+func TestMeta_Marshal(t *testing.T) {
+	m := Meta{
+		Id:   123,
+		Type: 1,
+		Name: "haha",
+		Tags: map[string]string{"os": "ubuntu"},
+	}
+	b, err := m.Marshal()
+	if err != nil {
+		t.Fatal("can't marshal to bytes")
+	}
+	m2 := Meta{}
+	err = m2.Unmarshal(b)
+	if err != nil {
+		t.Fatal("can't unmarshal from bytes")
+	}
+	if m.Id != m2.Id {
+		t.Fatal("should match")
+	}
+}
+
+func TestIntSeries_Marshal(t *testing.T) {
+	intS := IntSeries{
+		Meta: Meta{
+			Id:   123,
+			Type: 1,
+			Name: "haha",
+			Tags: map[string]string{"os": "ubuntu"},
+		},
+		Points: []IntPoint{{T: 1, V: 1}, {T: 1, V: 2}},
+	}
+	b, err := intS.Marshal()
+	if err != nil {
+		t.Fatal("can't marshal int series")
+	}
+	intS2 := IntSeries{}
+	err = intS2.Unmarshal(b)
+	if err != nil {
+		t.Fatal("can't unmarshal int series")
+	}
+	if intS.Meta.Id != intS2.Meta.Id {
+		t.Fatal("should match")
+	}
+}
