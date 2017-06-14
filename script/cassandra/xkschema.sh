bf121c499e22f1e83484d38d57599a5d386e10d6
at15 <at15@outlook.com>
2017-06-14 14:47:18 -0700

    [bench] Mount volume for C* and InfluxDB #51

--- script/cassandra/xkschema.sh
+++ script/cassandra/xkschema.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "create cassandra schema for xephon-k"
-xkd schema --cassandra-host localhost
-xkd schema --cassandra-host localhost
-xkd schema --cassandra-host localhost
+xkd schema --config ../../xkd.yml
+xkd schema --config ../../xkd.yml
+xkd schema --config ../../xkd.yml
 echo "schema created"
--- script/influxdb/run_influxdb.sh
+++ script/influxdb/run_influxdb.sh
@@ -2,4 +2,4 @@
 
 docker start tsdb-influxdb
 sleep 5
-curl -XPOST 'http://localhost:8086/query?u=myusername&p=mypassword' --data-urlencode 'q=CREATE DATABASE "sb"'
+curl -XPOST 'http://localhost:8086/query?u=myusername&p=mypassword' --data-urlencode 'q=CREATE DATABASE "xb"'
