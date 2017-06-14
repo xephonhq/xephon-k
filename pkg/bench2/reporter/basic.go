db348d79ea20682fb3458350871b0b7b5f2da308
at15 <at15@outlook.com>
2017-06-14 15:19:25 -0700

    [bench][report] Record overall execution time
    - this is needed for limit by number of points
    - However, the pure disk backend is even faster than the pure memory backend, maybe
    due to index and mutex.
    - Also if we have more concurrent clients, the lock on disk writer
    should be worse than the lock on memory because it is simply locking
    everything

--- pkg/bench2/reporter/basic.go
+++ pkg/bench2/reporter/basic.go
@@ -3,12 +3,15 @@ package reporter
 import (
 	"context"
 	"fmt"
+	"time"
 	"github.com/VividCortex/gohistogram"
 	"github.com/xephonhq/xephon-k/pkg/client"
 )
 
 type BasicReporter struct {
 	counter           int64
+	start             time.Time
+	end               time.Time
 	fastest           int64
 	slowest           int64
 	totalRequestSize  int64
@@ -18,21 +21,23 @@ type BasicReporter struct {
 }
 
 func (b *BasicReporter) Run(ctx context.Context, c chan *client.Result) {
+	b.start = time.Now()
 	b.slowest = 0
 	b.fastest = 99999999999
 	b.statusCode = make(map[int]int64, 10)
 	b.hisogram = gohistogram.NewHistogram(20)
+LOOP:
 	for {
 		select {
 		case <-ctx.Done():
 			log.Info("basic report finished by context")
-			return
+			break LOOP
 		case result, ok := <-c:
 			// FIXED: this is never triggered?
 			// The parent goroutine should sleep for a while so reporter can drain the channel
 			if !ok {
 				log.Info("basic report finished by channel")
-				return
+				break LOOP
 			}
 			// NOTE: since reporter is accessed by only one goroutine, these operation should be safe
 			d := result.End.Sub(result.Start).Nanoseconds()
@@ -50,10 +55,12 @@ func (b *BasicReporter) Run(ctx context.Context, c chan *client.Result) {
 			b.hisogram.Add(result.End.Sub(result.Start).Seconds())
 		}
 	}
+	b.end = time.Now()
 }
 
 func (b *BasicReporter) Finalize() {
 	fmt.Print(b.hisogram.String())
+	log.Infof("run time %f s", b.end.Sub(b.start).Seconds())
 	log.Infof("total request %d", b.counter)
 	log.Infof("fastest %d", b.fastest)
 	log.Infof("slowest %d", b.slowest)
--- pkg/bench2/reporter/discard.go
+++ pkg/bench2/reporter/discard.go
@@ -3,35 +3,42 @@ package reporter
 import (
 	"context"
 	"github.com/xephonhq/xephon-k/pkg/client"
+	"time"
 )
 
 // DiscardReporter is used to drain from channel only, it reports to nowhere
 type DiscardReporter struct {
 	counter int64
+	start   time.Time
+	end     time.Time
 }
 
 // Run implements Reporter
 func (n *DiscardReporter) Run(ctx context.Context, c chan *client.Result) {
+	n.start = time.Now()
+LOOP:
 	for {
 		select {
 		case <-ctx.Done():
 			log.Info("null report finished by context")
-			return
+			break LOOP
 		case _, ok := <-c:
 			// FIXED: this is never triggered?
 			// The parent goroutine should sleep for a while so reporter can drain the channel
 			if !ok {
 				log.Info("null report finished by channel")
-				return
+				break LOOP
 			}
 			// just drain from the channel and do nothing
 			// NOTE: since null reporter is accessed by only one goroutine, this operation should be safe
 			n.counter++
 		}
 	}
+	n.end = time.Now()
 }
 
 func (n *DiscardReporter) Finalize() {
+	log.Infof("run time %d s", n.end.Sub(n.start).Seconds())
 	log.Infof("total request %d", n.counter)
 	log.Info("null reporter has nothing to say")
 }
